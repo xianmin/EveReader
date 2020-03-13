0e0bd9d2e81c74673d62882848cfb50c1af674c3
Chen Xianmin <xianmin12@gmail.com>
2020-03-13 14:34:34 +0800

    add element-ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="home">
-    <eve-viewer v-if='isReady'></eve-viewer>
+    <el-container>
+
+      <el-aside>
+        <div id="activitybar">
+          <div class="sidebar-icon" @click="openFile">
+            open
+          </div>
+        </div>
+      </el-aside>
+
+      <el-container>
+        <el-main>
+          <div id="eve-reader-view"></div>
+        </el-main>
+      </el-container>
+
+    </el-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Epub from 'epubjs'
-import EveViewer from '@/components/EveViewer';
 
 global.epub = Epub
 
@@ -15,7 +30,6 @@ export default {
   name: 'home',
 
   components: {
-    EveViewer,
   },
 
   data() {
@@ -25,6 +39,60 @@ export default {
   },
 
   mounted() {
-  }
+    this.initViewer();
+  },
+
+  methods: {
+    initViewer() {
+      this.book = new Epub();
+    },
+    
+    openFile() {
+      var fi = document.createElement("input");
+      fi.setAttribute("accept", "application/epub+zip");
+      fi.style.display = "none";
+      fi.type = "file";
+      fi.onchange = () => {
+        var reader = new FileReader();
+        reader.onload = () => {
+          console.log(this.book);
+          this.openEpub(reader.result);
+        }
+        if (fi.files[0]) {
+            reader.readAsArrayBuffer(fi.files[0]);
+        }
+      };
+      document.body.appendChild(fi);
+      fi.click();
+    },
+
+    openEpub(data) {
+      this.book.open(data, "binary");
+      this.rendition = this.book.renderTo("eve-reader-view", {
+        manager: "continuous",
+        flow: "scrolled",
+        axis: "vertical",
+        width: "100%",
+        height: "100%",
+        // snap: true,
+        fullsize: true,
+      });
+      this.rendition.display("epubcfi(/6/14[id122]!/4/20/1:145)");
+    },
+  },
 }
 </script>
+
+<style scoped>
+#activitybar {
+  width: 60px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  /* background-color: #2c2c2c; */
+  top: 0px;
+  bottom: 0px;
+  left: 0px;
+  position: fixed;
+}
+</style>
