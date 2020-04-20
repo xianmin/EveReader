ff0b28d8e9018ca55c1b09af3d4f0e5fe8450ae1
Chen Xianmin <xianmin12@gmail.com>
2020-04-20 16:27:18 +0800

    add calibre env config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,12 @@
 module.exports = {
+  // "production" is for github page
   publicPath: process.env.NODE_ENV === 'production'
     ? '/EveReader/'
     : '/',
+  
+  // different outputDir is for eve-calibre-web project
+  outputDir: process.env.NODE_ENV === 'calibre' ? '../dist' : 'dist',
+
   chainWebpack: config => {
     config.module
       .rule("vue")
