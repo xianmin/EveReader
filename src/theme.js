82c8b02f7ac286e13fe8a14c52ee6178707d428d
Chen Xianmin <xianmin12@gmail.com>
2020-04-28 19:46:31 +0800

    style: fix reader line-height doesn't work at some epub

--- src/theme.js
+++ src/theme.js
@@ -4,7 +4,9 @@ class Theme{
     this.default = {
       body: {
         margin: '0 auto !important;',
-        padding: '0 0 60px 0 !important;',
+        'padding-bottom': '60px !important;',
+      },
+      p: {
       },
       '::selection': {
         'background': 'rgba(255,255,0, 0.3)'
@@ -14,6 +16,7 @@ class Theme{
 
   updateDefault(setting) {
     this.default.body['line-height'] = setting.lineHeight + '!important';
+    this.default.p['line-height'] = setting.lineHeight + '!important';
     this.default.body['max-width'] = setting.pageWidth + '!important';
     this.default.body['background-color'] = setting.backgroundColor + '!important;';
     this.rendition.themes.registerRules("default", this.default);
