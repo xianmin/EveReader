02d2dcc23e04f458c98c73d282c8e364c45eec91
Chen Xianmin <xianmin12@gmail.com>
2020-12-29 16:00:33 +0800

    eslint warn rule 'no-unused-vars'

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,15 @@ module.exports = {
     node: true,
     browser: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
   },
   rules: {
     // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-unused-vars": "warn",
+    "no-use-before-define": "warn",
   },
   ignorePatterns: ["/src/epubjs/**"],
-}
+};
