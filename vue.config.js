d1dbe3751db5c4df04783bcbf28b502c61fa066a
r0 <64012087+dominggo1999@users.noreply.github.com>
2023-06-02 23:46:26 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // "production" is for github page
-  publicPath: process.env.NODE_ENV === "production" ? "/EveReader/" : "/",
+  publicPath: "/",
 
   // different outputDir is for eve-calibre-web project
   outputDir: process.env.NODE_ENV === "calibre" ? "../dist" : "dist",
